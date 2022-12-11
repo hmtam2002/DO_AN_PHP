a4252660918bdf5305c607f36d4356eb738d0bc8
tuongvycute <0306201285@caothang.edu.vn>
2022-12-11 21:44:19 +0700

    .

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Faker\Provider\Image;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
---
+++ database/migrations/2022_12_11_074302_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_code')->unique();
+            $table->string('product_name')->unique();
+            $table->string('discription');
+            $table->float('price')->default(0);
+            $table->integer('brand_id')->unsigned()->index();
+            $table->integer('category-id')->unsigned()->index();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('brand_id')->references('id')->on('brand');
+            $table->foreign('category_id')->references('id')->on('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
